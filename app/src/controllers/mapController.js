d57a47bebdd8b233a6722cfbfb7786ffab69447e
Noraen Baryoun <n.baryoun@gmail.com>
2016-10-12 20:05:53 -0400

    added pictures, my posts page, and markers

--- app/src/controllers/mapController.js
+++ app/src/controllers/mapController.js
@@ -9,22 +9,47 @@
 module.exports = [
     '$scope',
     'GeoService',
-    function( $scope, GeoService )
+    '$firebaseArray',
+    '$ionicLoading',
+
+    function( $scope, GeoService, $firebaseArray, $ionicLoading )
     {
+      var ref = firebase.database().ref();
+      var postRef = ref.child('posts');
+      var postsArray = $firebaseArray(postRef);
+
+      postsArray.$loaded().then(function () {
+        console.log('posts: ', postsArray);
+        $scope.posts = postsArray;
+      });
+
         $scope.mapCreated= function(map){
           console.log('map was created');
           $scope.map = map;
-          $scope.loadMarkers();
+          postsArray.$loaded().then(function () {
+            $scope.loadMarkers();
+          });
         };
 
         $scope.loadMarkers = function () {
-
+          $scope.markers = [];
+          $scope.posts.forEach(function (post) {
+            var marker = new google.maps.Marker({
+              position: post.location,
+              map: $scope.map,
+              icon: post.picture
+            });
+            $scope.markers.push(marker);
+          });
+          console.log($scope.markers);
         };
 
         $scope.centerMap = function (){
+          $ionicLoading.show();
           console.log($scope.map);
           GeoService.getCurrentLocation().then(function () {
             $scope.map.setCenter(GeoService.currentLocation);
+            $ionicLoading.hide();
           });
         }
 
--- app/src/controllers/postController.js
+++ app/src/controllers/postController.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * @ngdoc function
  * @name FirstWeb.controller:PostController
@@ -7,8 +6,56 @@
  * # PostController
  */
 module.exports = [
+  '$scope',
+  '$firebaseArray',
+  '$state',
+  'currentAuth',
+  'GeoService',
+  '$ionicLoading',
 
-    function () {
-        // do something
+  function ($scope, $firebaseArray, $state, currentAuth, GeoService, $ionicLoading) {
+
+    var ref = firebase.database().ref();
+    var postRef = ref.child('posts').orderByChild('userid').equalTo(currentAuth.uid);
+    var postsArray = $firebaseArray(postRef);
+
+    postsArray.$loaded().then(function () {
+      console.log('posts: ', postsArray);
+      $scope.myPosts = postsArray;
+    });
+    $scope.newPost = {
+      picture: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiRq-rqttbPAhVnz1QKHYtJAmoQjRwIBQ&url=https%3A%2F%2Fplacehold.it%2F&psig=AFQjCNGPFwhgLpUmBuc3Ucu5PdRpeRnWug&ust=1476401638329373',
+      title: '',
+      comment: '',
+      location: {
+        lat: '',
+        lng: ''
+      },
+      userid: currentAuth.uid
+    };
+
+
+    $scope.savePost = function () {
+      $ionicLoading.show();
+      GeoService.getCurrentLocation().then(function () {
+        $scope.newPost.location = GeoService.currentLocation;
+        postsArray.$add($scope.newPost).then(function(ref) {
+            console.log(ref);
+            $ionicLoading.hide();
+            $state.go('app.map');
+          },
+          function(error) {
+            console.log("Error: ", error);
+            $ionicLoading.hide();
+          });
+      }, function (error) {
+        console.log("Error: ", error);
+        $ionicLoading.hide();
+      });
+
+    };
+    $scope.choiceSheet =function () {
+      //show the action sheet
     }
+  }
 ];
